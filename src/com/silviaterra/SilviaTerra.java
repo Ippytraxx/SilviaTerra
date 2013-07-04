0a0f82f44d33f912a1482729a5a37a37b09a3d3f
Ippytraxx <ippytraxx@github.com>
2013-07-04 17:35:53 +0200

    Complete rewrite

--- src/com/silviaterra/SilviaTerra.java
+++ src/com/silviaterra/SilviaTerra.java
@@ -2,8 +2,10 @@ package com.silviaterra;
 
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 
 /**
  *
@@ -11,228 +13,169 @@ import org.json.simple.parser.JSONParser;
  */
 public class SilviaTerra
 {
-    static double DBHLength;
+    static double DBHLength = 59;
     static double DBHPixels;
-    static double HFOV;
     static double VFOV;
-    static double HRes;
-    static double VRes;
-    static double crosshairModifier;
-    static double verticalTiltAngle;
-    
-    static String treeInfoFilePath;
-    static String globalInfoFilePath;
-    
+    static double HFOV;
+    static double verticalTiltAngle = 14.5;
+    static double HRes = 2448;
+    static double VRes = 3264;
+    static double highestDBHHeight;
+    static double imagePlateScale;
     static double horizontalDistance;
-    static double horizontalPixelDistance;
-    static double topDistance;
-    static double widthYPos;
-    static double widthPixelLength;
-    static double width;
-    static double height;
     
-    static double[][] pixelCoordinates;
-    static double[][] pixelDimensions;
-    static ArrayList<Section> treeSections = new ArrayList();
-    static ArrayList<ArrayList<Double>> pixelList = new ArrayList();
+    static ArrayList<HashMap<String, ArrayList<Double>>> rawDBHPixelList;
+    static ArrayList<HashMap<String, ArrayList<Double>>> rawBolePixelList;
+    static ArrayList<ArrayList<Double>> dbhPixelList = new ArrayList();
+    static ArrayList<ArrayList<Double>> bolePixelList = new ArrayList();
     
     public static void main(String[] args)
     {
-        treeInfoFilePath = args[0];
-        globalInfoFilePath = args[1];
-        
-        /*
-         * User set variables
-         */
-        DBHLength = 59;
-        DBHPixels = 283;
-        HFOV= 47.1;
-        VFOV = 60.5;
-        HRes = 2448;
-        VRes = 3264;
-        verticalTiltAngle = 18;
-        
-//        double[] widths = new double[]{177,299,511,968};
-//        double[] heights = new double[]{1667,1316,739,112};
-        
         JSONParser jsonParser = new JSONParser();
         
         try
         {
-            Object obj = jsonParser.parse(new FileReader(treeInfoFilePath));
+            Object obj = jsonParser.parse(new FileReader("/home/vincent/Development/Java_Projects/SilviaTerra/examples/global_info.txt"));
             JSONObject jsonObject1 = (JSONObject) obj;
             
-            obj = jsonParser.parse(new FileReader(globalInfoFilePath));
+            obj = jsonParser.parse(new FileReader("/home/vincent/Development/Java_Projects/SilviaTerra/examples/tree_info.txt"));
             JSONObject jsonObject2 = (JSONObject) obj;
             
-            DBHLength = (Double.parseDouble((String) jsonObject1.get("dbh"))) * 2.54;
-            VFOV = (double) jsonObject2.get("horizontalViewAngle");
-            HFOV = (double) jsonObject2.get("verticalViewAngle");
+            obj = jsonParser.parse(new FileReader("/home/vincent/Development/Java_Projects/SilviaTerra/examples/pixels1.txt"));
+            JSONObject jsonObject3 = (JSONObject) obj;
+            
+//            DBHLength = (Double.parseDouble((String) jsonObject2.get("dbh"))) * 2.54;
+            VFOV = (double) jsonObject1.get("horizontalViewAngle");
+            HFOV = (double) jsonObject1.get("verticalViewAngle");
+//            verticalTiltAngle = ((double) (jsonObject1.get("bolePitch"))) + 90.0;
+            rawDBHPixelList = (ArrayList) jsonObject3.get("dbh");
+            rawBolePixelList = (ArrayList) jsonObject3.get("bole");
+            
+            getDBHPixels();
+            getHorizontalDistance();
+            getImagePlateScale();
+            getPixelWidths(rawDBHPixelList, dbhPixelList);
+            getPixelHeights(rawDBHPixelList, dbhPixelList, true);
+            getHeights(dbhPixelList, true);
+            getWidths(dbhPixelList);
+            
+            getHighestDBHHeight();
+            getPixelWidths(rawBolePixelList, bolePixelList);
+            getPixelHeights(rawBolePixelList, bolePixelList, false);
+            getHeights(bolePixelList, false);
+            getWidths(bolePixelList);
+            
+            System.out.println(horizontalDistance);
+            for(int i = 0; i < bolePixelList.size(); i++)
+            {
+                System.out.println(bolePixelList.get(i).get(0) + " : " + bolePixelList.get(i).get(1) + " : " + bolePixelList.get(i).get(2) + " : " + bolePixelList.get(i).get(3) + " : " + bolePixelList.get(i).get(4));
+            }
+            
+            for(int i = 0; i < dbhPixelList.size(); i++)
+            {
+                System.out.println(dbhPixelList.get(i).get(0) + " : " + dbhPixelList.get(i).get(1) + " : " + dbhPixelList.get(i).get(2) + " : " + dbhPixelList.get(i).get(3) + " : " + dbhPixelList.get(i).get(4));
+            }
+            
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
-        
-        for(int i = 0; i < 6; i++)
+    }
+    
+    public static void getDBHPixels()
+    {
+        for(int i = 0; i < rawDBHPixelList.size(); i++)
         {
-            pixelList.add(new ArrayList<Double>());
-        }
-        
-        pixelList.get(0).add(1247.0);
-        pixelList.get(0).add(1191.0);
-        pixelList.get(0).add(1636.0);
-        
-        pixelList.get(1).add(1282.0);
-        pixelList.get(1).add(1151.0);
-        pixelList.get(1).add(1709.0);
-        
-        pixelList.get(2).add(1305.0);
-        pixelList.get(2).add(1129.0);
-        pixelList.get(2).add(1785.0);
-        
-        pixelList.get(3).add(1319.0);
-        pixelList.get(3).add(1112.0);
-        pixelList.get(3).add(1854.0);
-        
-        pixelList.get(4).add(1338.0);
-        pixelList.get(4).add(1090.0);
-        pixelList.get(4).add(1937.0);
-        
-        pixelList.get(5).add(1363.0);
-        pixelList.get(5).add(1069.0);
-        pixelList.get(5).add(2001.0);
-                
-        getHorizontalDistance();
-        getHorizontalPixelDistance();
-        getTopDistance();
-        
-        System.out.println(horizontalDistance);
-        System.out.println(horizontalPixelDistance);
-        System.out.println();
-        
-        for(int i = 0; i < pixelList.size(); i++)
-        {
-            getPixelHeights(i);
-            getPixelWidths(i);
-
-            getWidth(i);
-            getHeight(i);
-            
-            System.out.println(pixelList.get(i).get(1) + " : " + pixelList.get(i).get(2) + " : " + pixelList.get(i).get(3) + " : " + pixelList.get(i).get(4) + " : " + pixelList.get(i).get(5));
-            
-            if(i % 2 == 0 && i != 0)
+            if(rawDBHPixelList.get(i).containsKey("dbh"))
             {
-                Section section = new Section(pixelList, i / 2);
-                section.getVolumeShape();
-                System.out.println(section.getVolume());
-                treeSections.add(section);
+                DBHPixels = rawDBHPixelList.get(i).get("right").get(0) - rawDBHPixelList.get(i).get("left").get(0);
             }
-        }   
+        }
+    }
+    
+    public static void getPixelWidths(ArrayList<HashMap<String, ArrayList<Double>>> list, ArrayList<ArrayList<Double>> list1)
+    {
+        for(int i = 0; i < list.size(); i++)
+        {
+            list1.add(new ArrayList());
+            list1.get(i).add(list.get(i).get("right").get(0) - list.get(i).get("left").get(0));
+        }
+    }
+    
+    public static void getPixelHeights(ArrayList<HashMap<String, ArrayList<Double>>> list, ArrayList<ArrayList<Double>> list1, boolean isDBH)
+    {
+        if(isDBH)
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                list1.get(i).add((VRes / 2) - list.get(i).get("right").get(1));
+            }
+        }
+        else
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                list1.get(i).add(VRes - list.get(i).get("right").get(1));
+            }
+        }
+    }
+    
+    public static void getWidths(ArrayList<ArrayList<Double>> list)
+    {
+        double distanceToWidth;
+        double imageCmPerPx;
+        
+        for(int i = 0; i < list.size(); i++)
+        {
+            distanceToWidth = horizontalDistance / Math.cos(Math.toRadians(list.get(i).get(2)));
+            imageCmPerPx = ((Math.tan(Math.toRadians(HFOV / 2)) * distanceToWidth) * 2) / HRes;
+            list.get(i).add(imageCmPerPx * list.get(i).get(0));
+        }
+    }
+    
+    public static void getHeights(ArrayList<ArrayList<Double>> list, boolean isDBH)
+    {
+        double angleToHeight;
+        
+        if(isDBH)
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                list.get(i).add(list.get(i).get(1) / imagePlateScale);
+                list.get(i).add(Math.tan(Math.toRadians(list.get(i).get(2))) * horizontalDistance);
+            }
+        }
+        else
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                list.get(i).add((list.get(i).get(1) / imagePlateScale) + (verticalTiltAngle - (VFOV / 2)));
+                if(Math.tan(Math.toRadians(list.get(i).get(2))) * horizontalDistance > highestDBHHeight * 1.1)
+                {
+                    list.get(i).add(Math.tan(Math.toRadians(list.get(i).get(2))) * horizontalDistance);
+                }
+                else
+                {
+                    list.remove(i);
+                    i--;
+                }
+            }
+        }
+    }
+    
+    public static void getHighestDBHHeight()
+    {
+        highestDBHHeight = dbhPixelList.get(dbhPixelList.size() - 1).get(3);
+    }
+    
+    public static void getImagePlateScale()
+    {
+        imagePlateScale = VRes / VFOV;
     }
     
     public static void getHorizontalDistance()
     {
-        double imageWidth;
-        
-        imageWidth = (DBHLength / DBHPixels) * HRes;
-        horizontalDistance = (imageWidth / 2) / Math.tan(Math.toRadians(HFOV / 2));
-    }
-    
-    public static void getHorizontalPixelDistance()
-    {
-        if(verticalTiltAngle > (VFOV / 2))
-        {
-            double topAngle;
-            double bisectionLengthPixels;
-            
-            topAngle = 180 - 90 - verticalTiltAngle;
-            bisectionLengthPixels = Math.sin(Math.toRadians(topAngle)) / (Math.sin(Math.toRadians(VFOV / 2)) / (VRes / 2));
-            
-            horizontalPixelDistance = bisectionLengthPixels * Math.cos(Math.toRadians(verticalTiltAngle - (VFOV / 2)));
-        }
-        else
-        {
-            horizontalPixelDistance = (VRes / 2) / Math.tan(Math.toRadians(verticalTiltAngle));
-        }
-    }
-    
-    public static void getTopDistance()
-    {   
-        topDistance = horizontalDistance / Math.cos(Math.toRadians(verticalTiltAngle));
-    }
-    
-    public static void getWidth(int i)
-    {
-        double missingVerticalPixelDistance;
-        double angleToWidth;
-        double distanceToWidth;
-        double imageWidth;
-        
-        widthYPos = pixelList.get(i).get(3);
-        widthPixelLength = pixelList.get(i).get(2);
-
-        if(verticalTiltAngle > (VFOV / 2))
-        {
-            missingVerticalPixelDistance = horizontalPixelDistance * Math.tan(Math.toRadians(verticalTiltAngle - (VFOV / 2)));
-            widthYPos = widthYPos + missingVerticalPixelDistance;
-
-            angleToWidth = Math.toDegrees(Math.atan(widthYPos / horizontalPixelDistance));
-
-            distanceToWidth = horizontalDistance / Math.cos(Math.toRadians(angleToWidth));
-
-            imageWidth = (distanceToWidth * Math.tan(Math.toRadians(HFOV / 2))) * 2;
-
-            pixelList.get(i).add((imageWidth / HRes) * widthPixelLength);
-        }
-        else
-        {
-            angleToWidth = Math.toDegrees(Math.atan(widthYPos / horizontalPixelDistance));
-
-            distanceToWidth = horizontalDistance / Math.cos(Math.toRadians(angleToWidth));
-
-            imageWidth = (distanceToWidth * Math.tan(Math.toRadians(HFOV / 2))) * 2;
-
-            pixelList.get(i).add((imageWidth / HRes) * widthPixelLength);
-        }
-    } 
-    
-    public static void getHeight(int i)
-    {
-        double missingVerticalPixelDistance;
-        double angleToWidth;
- 
-        widthYPos = pixelList.get(i).get(3);
-        
-        if(verticalTiltAngle > (VFOV / 2))
-        {
-            missingVerticalPixelDistance = horizontalPixelDistance * Math.tan(Math.toRadians(verticalTiltAngle - (VFOV / 2)));
-            widthYPos = widthYPos + missingVerticalPixelDistance;
-
-//            angleToWidth = Math.toDegrees(Math.atan(widthYPos / horizontalPixelDistance));
-//
-//            pixelList.get(i).add(Math.tan(Math.toRadians(angleToWidth)) * horizontalDistance);
-            
-            pixelList.get(i).add(((Math.tan(Math.toRadians(verticalTiltAngle)) * horizontalDistance) / (pixelList.get(0).get(3)) + missingVerticalPixelDistance) * (widthYPos +  missingVerticalPixelDistance));
-            
-        }
-        else
-        {
-//            angleToWidth = Math.toDegrees(Math.atan(widthYPos / horizontalPixelDistance));
-//
-//            pixelList.get(i).add(Math.tan(Math.toRadians(angleToWidth)) * horizontalDistance);
-            
-            pixelList.get(i).add(((Math.tan(Math.toRadians(verticalTiltAngle)) * horizontalDistance) / pixelList.get(0).get(3)) * widthYPos);
-        }
-    }
-    
-    public static void getPixelWidths(int i)
-    {
-        pixelList.get(i).set(2, pixelList.get(i).get(0) - pixelList.get(i).get(1));
-    }
-    
-    public static void getPixelHeights(int i)
-    {
-        pixelList.get(i).add(VRes - pixelList.get(i).get(2));
+        horizontalDistance = (((DBHLength / DBHPixels) * HRes) / 2) / Math.tan(Math.toRadians(HFOV / 2));
     }
 }
